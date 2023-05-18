6282196ae0411925a0c0fa43ccbb535949e56e49
Vipul Singh <120732585+Vipul1305@users.noreply.github.com>
2023-05-18 19:55:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+let team = players;
+let team1 = JSON.prase(JSON.stringify(players));
+
+let cap1 = JSON.prase(JSON.stringify(person))
+
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
